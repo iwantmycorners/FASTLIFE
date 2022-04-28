086ad2a7f6b6f62bd04e77574779a7ac325148ae
julien kiang <kiang.jn@gmail.com>
2022-04-28 09:39:14 +0200

    ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ require "net/http"
 
 class PagesController < ApplicationController
   def home
-    url = URI("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=48.87%2C2.38&radius=1000&type=restaurant&key=AIzaSyAuDFBQoAoPnBnOLQJB6_XQyUFfMW7v8C0")
+    url = URI("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=48.87%2C2.38&radius=1000&type=restaurant&key=")
 
     https = Net::HTTP.new(url.host, url.port)
     https.use_ssl = true
